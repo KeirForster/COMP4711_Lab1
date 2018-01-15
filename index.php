140961443d8e307e2f615bc1dfa1268f06dc7abf
Keir-Laptop <Keir-Laptop@192.168.0.178>
2018-01-14 16:04:44 -0800

    Index page with 3 sample students in sorted order

--- index.php
+++ index.php
@@ -11,10 +11,11 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            include('Student.php');
+            include('Student.php'); // include the student php class
             
-            $students = array();
+            $students = array(); // create an array for student objects
             
+            // add a student
             $first = new Student();
             $first->surname = "Doe";
             $first->first_name = "John";
@@ -25,6 +26,7 @@ and open the template in the editor.
             $first->add_grade(55);
             $students['j123'] = $first;
             
+            // add a second student
             $second = new Student();
             $second->surname = "Einstein";
             $second->first_name = "Albert";
@@ -36,6 +38,19 @@ and open the template in the editor.
             $second->add_grade(50);
             $students['a456'] = $second;
             
+            // add a third student
+            $third = new Student();
+            $third->surname = "Forster";
+            $third->first_name = "Keir";
+            $third->add_email('work','kforster8@my.bcit.ca');
+            $third->add_grade(70);
+            $third->add_grade(80);
+            $third->add_grade(90);
+            $students['k771'] = $third;
+            
+            ksort($students); // sort the student objects based on their key
+            
+            // print each student object to the page
             foreach($students as $student)
                 echo $student->toString();
         ?>
